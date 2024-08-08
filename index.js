ec66f0fe17dc47e2a478b4ffbca46448897834e9
RWD0327 <kjw46220@outlook.com>
2024-08-08 10:48:48 +0000

    chore: Update header styles and add main content to index.html

--- index.js
+++ index.js
@@ -24,3 +24,7 @@ function handleResize() {
 		sidebar.style.display = 'none';
 	}
 }
+
+const texts = ['Text 1', 'Text 2', 'Text 3', 'Text 4', 'Text 5'];
+const randomText = texts[Math.floor(Math.random() * texts.length)];
+console.log(randomText);

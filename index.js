33a14e8d87a978bb992bdb53ff425b27d01b3526
RWD0327 <kjw46220@outlook.com>
2024-10-12 02:32:08 +0000

    Refactor header and menu styles in index.css, update logo in index.html, and add JavaScript functionality to toggle hidden sidebar in index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementsByid('menu_bar').addEventListener('click', function(){
+    var sidebar = document.getElementsByid('hidden_sidebar');
+
+sidebar.classList.toggle('active');
+
+})

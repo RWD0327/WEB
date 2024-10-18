bdd0d5b3b1bb594a7c875ff2ca9e53271ddd5f09
RWD0327 <kjw46220@outlook.com>
2024-10-18 10:17:11 +0000

    Refactor index.js and index.css

--- index.js
+++
@@ -1,6 +0,0 @@
-document.getElementsByid('menu_bar').addEventListener('click', function(){
-    var sidebar = document.getElementsByid('hidden_sidebar');
-
-sidebar.classList.toggle('active');
-
-})
